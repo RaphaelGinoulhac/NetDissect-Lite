de62760e30fe353980c61efc205456488a29bdd8
Raphael <raphael.ginoulhac@gmail.com>
2019-06-19 10:34:56 -0400

    Reformat code, and added a if batch is None condition, so that the program doesn't encounter a StopIteration

--- feature_operation.py
+++ feature_operation.py
@@ -1,4 +1,3 @@
-
 import os
 from torch.autograd import Variable as V
 from scipy.misc import imresize
@@ -13,6 +12,8 @@ from loader.data_loader import load_csv
 from loader.data_loader import SegmentationData, SegmentationPrefetcher
 
 features_blobs = []
+
+
 def hook_feature(module, input, output):
     features_blobs.append(output.data.cpu().numpy())
 
@@ -23,8 +24,8 @@ class FeatureOperator:
         if not os.path.exists(settings.OUTPUT_FOLDER):
             os.makedirs(os.path.join(settings.OUTPUT_FOLDER, 'image'))
         self.data = SegmentationData(settings.DATA_DIRECTORY, categories=settings.CATAGORIES)
-        self.loader = SegmentationPrefetcher(self.data,categories=['image'],once=True,batch_size=settings.BATCH_SIZE)
-        self.mean = [109.5388,118.6897,124.6901]
+        self.loader = SegmentationPrefetcher(self.data, categories=['image'], once=True, batch_size=settings.BATCH_SIZE)
+        self.mean = [109.5388, 118.6897, 124.6901]
 
     def feature_extraction(self, model=None, memmap=True):
         loader = self.loader
@@ -36,16 +37,18 @@ class FeatureOperator:
 
         if memmap:
             skip = True
-            mmap_files =  [os.path.join(settings.OUTPUT_FOLDER, "%s.mmap" % feature_name)  for feature_name in  settings.FEATURE_NAMES]
-            mmap_max_files = [os.path.join(settings.OUTPUT_FOLDER, "%s_max.mmap" % feature_name) for feature_name in settings.FEATURE_NAMES]
+            mmap_files = [os.path.join(settings.OUTPUT_FOLDER, "%s.mmap" % feature_name) for feature_name in
+                          settings.FEATURE_NAMES]
+            mmap_max_files = [os.path.join(settings.OUTPUT_FOLDER, "%s_max.mmap" % feature_name) for feature_name in
+                              settings.FEATURE_NAMES]
             if os.path.exists(features_size_file):
                 features_size = np.load(features_size_file)
             else:
                 skip = False
-            for i, (mmap_file, mmap_max_file) in enumerate(zip(mmap_files,mmap_max_files)):
+            for i, (mmap_file, mmap_max_file) in enumerate(zip(mmap_files, mmap_max_files)):
                 if os.path.exists(mmap_file) and os.path.exists(mmap_max_file) and features_size[i] is not None:
                     print('loading features %s' % settings.FEATURE_NAMES[i])
-                    wholefeatures[i] = np.memmap(mmap_file, dtype=float,mode='r', shape=tuple(features_size[i]))
+                    wholefeatures[i] = np.memmap(mmap_file, dtype=float, mode='r', shape=tuple(features_size[i]))
                     maxfeatures[i] = np.memmap(mmap_max_file, dtype=float, mode='r', shape=tuple(features_size[i][:2]))
                 else:
                     print('file missing, loading from scratch')
@@ -54,19 +57,22 @@ class FeatureOperator:
                 return wholefeatures, maxfeatures
 
         num_batches = (len(loader.indexes) + loader.batch_size - 1) / loader.batch_size
-        for batch_idx,batch in enumerate(loader.tensor_batches(bgr_mean=self.mean)):
+        for batch_idx, batch in enumerate(loader.tensor_batches(bgr_mean=self.mean)):
+            # modified, testing the is None condition here rather than in the generator, which would raise an exception and terminate the program
+            if batch is None:
+                break
             del features_blobs[:]
             input = batch[0]
             batch_size = len(input)
-            print('extracting feature from batch %d / %d' % (batch_idx+1, num_batches))
+            print('extracting feature from batch %d / %d' % (batch_idx + 1, num_batches))
             input = torch.from_numpy(input[:, ::-1, :, :].copy())
             input.div_(255.0 * 0.224)
             if settings.GPU:
                 input = input.cuda()
-            input_var = V(input,volatile=True)
+            input_var = V(input, volatile=True)
             logit = model.forward(input_var)
             while np.isnan(logit.data.cpu().max()):
-                print("nan") #which I have no idea why it will happen
+                print("nan")  # which I have no idea why it will happen
                 del features_blobs[:]
                 logit = model.forward(input_var)
             if maxfeatures[0] is None:
@@ -74,33 +80,33 @@ class FeatureOperator:
                 for i, feat_batch in enumerate(features_blobs):
                     size_features = (len(loader.indexes), feat_batch.shape[1])
                     if memmap:
-                        maxfeatures[i] = np.memmap(mmap_max_files[i],dtype=float,mode='w+',shape=size_features)
+                        maxfeatures[i] = np.memmap(mmap_max_files[i], dtype=float, mode='w+', shape=size_features)
                     else:
                         maxfeatures[i] = np.zeros(size_features)
             if len(feat_batch.shape) == 4 and wholefeatures[0] is None:
                 # initialize the feature variable
                 for i, feat_batch in enumerate(features_blobs):
                     size_features = (
-                    len(loader.indexes), feat_batch.shape[1], feat_batch.shape[2], feat_batch.shape[3])
+                        len(loader.indexes), feat_batch.shape[1], feat_batch.shape[2], feat_batch.shape[3])
                     features_size[i] = size_features
                     if memmap:
                         wholefeatures[i] = np.memmap(mmap_files[i], dtype=float, mode='w+', shape=size_features)
                     else:
                         wholefeatures[i] = np.zeros(size_features)
             np.save(features_size_file, features_size)
-            start_idx = batch_idx*settings.BATCH_SIZE
-            end_idx = min((batch_idx+1)*settings.BATCH_SIZE, len(loader.indexes))
+            start_idx = batch_idx * settings.BATCH_SIZE
+            end_idx = min((batch_idx + 1) * settings.BATCH_SIZE, len(loader.indexes))
             for i, feat_batch in enumerate(features_blobs):
                 if len(feat_batch.shape) == 4:
                     wholefeatures[i][start_idx:end_idx] = feat_batch
-                    maxfeatures[i][start_idx:end_idx] = np.max(np.max(feat_batch,3),2)
+                    maxfeatures[i][start_idx:end_idx] = np.max(np.max(feat_batch, 3), 2)
                 elif len(feat_batch.shape) == 3:
                     maxfeatures[i][start_idx:end_idx] = np.max(feat_batch, 2)
                 elif len(feat_batch.shape) == 2:
                     maxfeatures[i][start_idx:end_idx] = feat_batch
         if len(feat_batch.shape) == 2:
             wholefeatures = maxfeatures
-        return wholefeatures,maxfeatures
+        return wholefeatures, maxfeatures
 
     def quantile_threshold(self, features, savepath=''):
         qtpath = os.path.join(settings.OUTPUT_FOLDER, savepath)
@@ -120,7 +126,7 @@ class FeatureOperator:
             batch = features[i:i + batch_size]
             batch = np.transpose(batch, axes=(0, 2, 3, 1)).reshape(-1, features.shape[1])
             quant.add(batch)
-        ret = quant.readout(1000)[:, int(1000 * (1-settings.QUANTILE)-1)]
+        ret = quant.readout(1000)[:, int(1000 * (1 - settings.QUANTILE) - 1)]
         if savepath:
             np.save(qtpath, ret)
         return ret
@@ -139,6 +145,8 @@ class FeatureOperator:
         start_time = time.time()
         last_batch_time = start_time
         for batch in pd.batches():
+            if batch is None:
+                return None
             batch_time = time.time()
             rate = (count - start) / (batch_time - start_time + 1e-15)
             batch_rate = len(batch) / (batch_time - last_batch_time + 1e-15)
@@ -172,8 +180,8 @@ class FeatureOperator:
                     feature_map = features[img_index][unit_id]
                     if feature_map.max() > threshold[unit_id]:
                         mask = imresize(feature_map, (concept_map['sh'], concept_map['sw']), mode='F')
-                        #reduction = int(round(settings.IMG_SIZE / float(concept_map['sh'])))
-                        #mask = upsample.upsampleL(fieldmap, feature_map, shape=(concept_map['sh'], concept_map['sw']), reduction=reduction)
+                        # reduction = int(round(settings.IMG_SIZE / float(concept_map['sh'])))
+                        # mask = upsample.upsampleL(fieldmap, feature_map, shape=(concept_map['sh'], concept_map['sw']), reduction=reduction)
                         indexes = np.argwhere(mask > threshold[unit_id])
 
                         tally_units[unit_id] += len(indexes)
@@ -181,13 +189,12 @@ class FeatureOperator:
                             tally_bt = np.bincount(pixels[:, indexes[:, 0], indexes[:, 1]].ravel())
                             if len(tally_bt) > 0:
                                 tally_bt[0] = 0
-                            tally_cat = np.dot(tally_bt[None,:], data.labelcat[:len(tally_bt), :])[0]
-                            tally_both[unit_id,:len(tally_bt)] += tally_bt
+                            tally_cat = np.dot(tally_bt[None, :], data.labelcat[:len(tally_bt), :])[0]
+                            tally_both[unit_id, :len(tally_bt)] += tally_bt
                         for scalar in scalars:
                             tally_cat += data.labelcat[scalar]
                             tally_both[unit_id, scalar] += len(indexes)
                         tally_units_cat[unit_id] += len(indexes) * (tally_cat > 0)
-
 
     def tally(self, features, threshold, savepath=''):
         csvpath = os.path.join(settings.OUTPUT_FOLDER, savepath)
@@ -197,25 +204,28 @@ class FeatureOperator:
         units = features.shape[1]
         labels = len(self.data.label)
         categories = self.data.category_names()
-        tally_both = np.zeros((units,labels),dtype=np.float64)
-        tally_units = np.zeros(units,dtype=np.float64)
-        tally_units_cat = np.zeros((units,len(categories)), dtype=np.float64)
-        tally_labels = np.zeros(labels,dtype=np.float64)
+        tally_both = np.zeros((units, labels), dtype=np.float64)
+        tally_units = np.zeros(units, dtype=np.float64)
+        tally_units_cat = np.zeros((units, len(categories)), dtype=np.float64)
+        tally_labels = np.zeros(labels, dtype=np.float64)
 
         if settings.PARALLEL > 1:
             psize = int(np.ceil(float(self.data.size()) / settings.PARALLEL))
             ranges = [(s, min(self.data.size(), s + psize)) for s in range(0, self.data.size(), psize) if
                       s < self.data.size()]
-            params = [(features, self.data, threshold, tally_labels, tally_units, tally_units_cat, tally_both) + r for r in ranges]
+            params = [(features, self.data, threshold, tally_labels, tally_units, tally_units_cat, tally_both) + r for r
+                      in ranges]
             threadpool = pool.ThreadPool(processes=settings.PARALLEL)
             threadpool.map(FeatureOperator.tally_job, params)
         else:
-            FeatureOperator.tally_job((features, self.data, threshold, tally_labels, tally_units, tally_units_cat, tally_both, 0, self.data.size()))
+            FeatureOperator.tally_job((features, self.data, threshold, tally_labels, tally_units, tally_units_cat,
+                                       tally_both, 0, self.data.size()))
 
         primary_categories = self.data.primary_categories_per_index()
         tally_units_cat = np.dot(tally_units_cat, self.data.labelcat.T)
-        iou = tally_both / (tally_units_cat + tally_labels[np.newaxis,:] - tally_both + 1e-10)
-        pciou = np.array([iou * (primary_categories[np.arange(iou.shape[1])] == ci)[np.newaxis, :] for ci in range(len(self.data.category_names()))])
+        iou = tally_both / (tally_units_cat + tally_labels[np.newaxis, :] - tally_both + 1e-10)
+        pciou = np.array([iou * (primary_categories[np.arange(iou.shape[1])] == ci)[np.newaxis, :] for ci in
+                          range(len(self.data.category_names()))])
         label_pciou = pciou.argmax(axis=2)
         name_pciou = [
             [self.data.name(None, j) for j in label_pciou[ci]]
@@ -228,7 +238,7 @@ class FeatureOperator:
         ordering = score_pciou.max(axis=0).argsort()[::-1]
         rets = [None] * len(ordering)
 
-        for i,unit in enumerate(ordering):
+        for i, unit in enumerate(ordering):
             # Top images are top[unit]
             bestcat = bestcat_pciou[0, unit]
             data = {
